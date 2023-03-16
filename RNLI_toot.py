b7343abb1a0fb9a9e1c00704da5923dd70a00316
SarahDal <46293785+SarahDal@users.noreply.github.com>
2023-03-16 17:47:00 +0000

    Create RNLI_toot.py
    created

---
+++ RNLI_toot.py
@@ -0,0 +1,62 @@
+import requests
+from datetime import datetime
+import os
+import sys
+from mastodon import Mastodon
+
+# Define the name of the file to write to
+filename = 'launch_time.txt'
+
+# Define the Mastodon instance URL and access token
+mastodon_url = 'https://botsin.space/'
+access_token = 'secret'
+
+# Create a Mastodon instance
+mastodon = Mastodon(
+    access_token=access_token,
+    api_base_url=mastodon_url
+)
+
+# Make a GET request to the API endpoint
+response = requests.get('https://services.rnli.org/api/launches')
+
+# Convert the response data to JSON format
+response_data = response.json()
+
+# Access the first launch in the response data
+first_launch = response_data[0]
+
+# Access the 'shortName' and 'launchDate' parameters for the first launch
+short_name = first_launch['shortName']
+
+# Parse the ISO 8601 formatted date string and convert it to a datetime object
+launch_datetime = datetime.fromisoformat(first_launch['launchDate'])
+
+# Format the datetime object as hh:mm and store it in a string variable
+launch_time = launch_datetime.strftime('%H:%M')
+
+
+# Check if the file already exists
+if os.path.isfile(filename):
+    # If the file exists, read its contents and compare to the new launch time
+    with open(filename, 'r') as file:
+        last_launch_time = file.read()
+    if last_launch_time == launch_time:
+        # If the launch time hasn't changed, exit the program without writing to the file
+        print('Launch time has not changed:', launch_time)
+        sys.exit()
+    else:
+        # If the launch time has changed, delete the existing file and continue
+        os.remove(filename)
+
+# Write the new launch time to the file
+with open(filename, 'w') as file:
+    file.write(launch_time)
+
+# Post to Mastodon with a status update
+status_update = f'Launched from {short_name} at {launch_time}'
+
+mastodon.status_post(status_update)
+
+# Print a confirmation message to the console
+print('Launch time has been updated to:', launch_time)
